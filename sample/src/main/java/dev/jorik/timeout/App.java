a627e56f1323cbc4d91eb1f44e173d70a4903cdd
JorikFat <jorikfat@gmail.com>
2022-10-29 19:54:04 +0300

    show timeout activity

---
+++ sample/src/main/java/dev/jorik/timeout/App.java
@@ -0,0 +1,19 @@
+package dev.jorik.timeout;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+import android.app.Application;
+import android.content.Intent;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startActivity(
+                new Intent(this, TimeoutActivity.class)
+                        .addFlags(FLAG_ACTIVITY_CLEAR_TASK | FLAG_ACTIVITY_NEW_TASK)
+        );
+    }
+}
